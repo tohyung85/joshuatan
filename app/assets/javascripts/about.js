a45282b0432f09fe00de38d9871635e1e94d9c7d
Joshua Tan <tantohyung@gmail.com>
2016-07-09 00:43:01 +0000

    Implement Image change script

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(".project").mouseover(function(){
+    var images = ['flixter.jpeg', 'todo.jpeg', 'gamershaunts.jpeg', 'gameoflife.png'];
+    var links = ["https://jt-flixter.herokuapp.com","https://jt-todo.herokuapp.com/","https://jt-gamershound.herokuapp.com","http://codepen.io/tohyung85/full/rxEZKy/"];
+    var id = $(this).data("id") - 1;
+    var imagePath = "/assets/" + images[id];
+    $(".project-image img").remove();
+    var replacement = "<a href="+links[id]+"><img src="+imagePath+"/></a>";
+    $(".project-image a").replaceWith(replacement);
+  });
+
+});
