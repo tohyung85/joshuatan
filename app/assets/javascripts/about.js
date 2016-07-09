df5854de9485c9968bf32e6bd4bc3040c7e7de56
Joshua Tan <tantohyung@gmail.com>
2016-07-09 09:06:26 +0000

    Set color and fix up loading circle

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,7 +1,7 @@
 $(function(){
   $("img").on('load', function(){
-    $(this).css('visibility', 'visible');
-    $(this).parent().children(".circularGcircle").hide();
+    $(this).show();
+    $(this).parent().children(".loading-circle").hide();
   });
 
   $(".project").mouseover(function(){
@@ -11,9 +11,10 @@ $(function(){
     var imagePath = "/assets/" + images[id];
     var replacement = "<a href='" + links[id] + "'><img src='" + imagePath + "'/></a>";
     $(".project-image").html(replacement);
-      $("img").on('load', function(){
-        $(this).css('visibility', 'visible');
-        $(this).parent().children(".circularGcircle").hide();
+    $(".project-image a").append('<div class="loading-circle"><div class="circularGcircle"><div id="circularG_1" class="circularG"></div><div id="circularG_2" class="circularG"></div><div id="circularG_3" class="circularG"></div><div id="circularG_4" class="circularG"></div><div id="circularG_5" class="circularG"></div><div id="circularG_6" class="circularG"></div><div id="circularG_7" class="circularG"></div><div id="circularG_8" class="circularG"></div></div></div>');
+      $(".project-image img").on('load', function(){
+        $(".project-image img").show();
+        $(this).parent().children(".loading-circle").hide();
       });
   });
 
