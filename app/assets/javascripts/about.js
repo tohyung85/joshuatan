d0acf0ca2c839ffcf769df04ab44ec495e45cec2
Joshua Tan <tantohyung@gmail.com>
2016-07-09 04:29:19 +0000

    Implement page specific javascript

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,4 +1,5 @@
 $(function(){
+  alert('hi');
   $(".project").mouseover(function(){
     var images = ['flixter.jpeg', 'todo.jpeg', 'gamershaunts.jpeg', 'gameoflife.png'];
     var links = ["https://jt-flixter.herokuapp.com","https://jt-todo.herokuapp.com/","https://jt-gamershound.herokuapp.com","http://codepen.io/tohyung85/full/rxEZKy/"];
@@ -7,7 +8,6 @@ $(function(){
     $(".project-image img").remove();
     var replacement = "<a href="+links[id]+"><img src="+imagePath+"/></a>";
     $(".project-image a").replaceWith(replacement);
-    alert('hi');
   });
 
 });
