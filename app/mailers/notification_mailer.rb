6371ec9a0763259985ce260ab0054966ec11f4a3
Joshua Tan <tantohyung@gmail.com>
2016-07-10 09:10:29 +0000

    Set up emailing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def contact_message_email(message)
+    @message = message
+    mail(from: @message.email, to: 'tantohyung@gmail.com', subject: "#{@message.name} has sent you a message!")
+  end
 end
