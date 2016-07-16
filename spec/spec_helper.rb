183d64f86568522f18f58ec7a0e9d6c53fa527a1
Joshua Tan <tantohyung@gmail.com>
2016-07-16 08:00:20 +0000

    Remove temp files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-
-
   config.after(:each) do
     if Rails.env.test? || Rails.env.cucumber?
       FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
